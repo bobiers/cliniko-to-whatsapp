2c52caa7572d75ebbc915da86c690d8b743cff17
Daniel Leong <danielleong47@gmail.com>
2024-08-28 17:30:31 +0800

    manage to use basic auth to connect the cliniko API

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
-require('dotenv').config();
-const axios = require('axios');
-const twilio = require('twilio');
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid,authToken);
-const whatsappNumber = process.env.TWILIO_WHATSAPP_NUMBER;
+import dotenv from 'dotenv';
+import axios from 'axios';
+// import twilio from 'twilio';
+
+dotenv.config();
+
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = twilio(accountSid,authToken);
+// const whatsappNumber = process.env.TWILIO_WHATSAPP_NUMBER;
 const clinikoApiKey = process.env.CLINIKO_API_KEY;
 
+const today = new Date() // get today's date
+const tomorrow = new Date(today)
+tomorrow.setDate(today.getDate() + 1) // Add 1 to today's date and set it to tomorrow
+const formatDate = (date) => date.toISOString().split('T')[0];
 
+const formattedToday = formatDate(today);
+const formattedTomorrow = formatDate(tomorrow);
+
+const fetchBookings = async () => {
+    try {
+
+        const query = new URLSearchParams({
+            order: 'asc',
+            page: '0',
+            per_page: '100',
+            'q[]': 'string',
+            sort: "starts_at",
+          }).toString();
+
+        const encodedApiKey = btoa(`${clinikoApiKey}:`);
+
+        const response = await axios.get(`https://api.au2.cliniko.com/v1/bookings?`, {
+            headers: {
+                'Authorization': `Basic ${encodedApiKey}`,
+                'Accept': 'application/json',
+                'User-Agent': 'Cliniko-to-Whatsapp API (ianchiro.frontdesk@gmail.com)',
+            },
+        });
+    
+        // Log the response data to see if the fetch is successful
+        console.log('Bookings fetched successfully:', response.data);
+        return response.data.bookings;
+    } catch (error) {
+        console.error('Error fetching bookings:', error.message);
+        return null;
+    }
+  };
+
+  const main = async () => {
+    await fetchBookings();
+  };
+  
+  main(); // Run the main function
