efaa08f7b8795e6cb2784966fb8e0b5cc62c05ba
Daniel Leong <danielleong47@gmail.com>
2024-08-30 17:50:58 +0800

    manage to fetch the data for tomorrow's date

--- app.js
+++ app.js
@@ -15,33 +15,29 @@ const tomorrow = new Date(today)
 tomorrow.setDate(today.getDate() + 1) // Add 1 to today's date and set it to tomorrow
 const formatDate = (date) => date.toISOString().split('T')[0];
 
-const formattedToday = formatDate(today);
 const formattedTomorrow = formatDate(tomorrow);
 
-const fetchBookings = async () => {
+const fetchBookings = async (endDate) => {
     try {
 
-        const query = new URLSearchParams({
-            order: 'asc',
-            page: '0',
-            per_page: '100',
-            'q[]': 'string',
-            sort: "starts_at",
-          }).toString();
-
-        const encodedApiKey = btoa(`${clinikoApiKey}:`);
-
-        const response = await axios.get(`https://api.au2.cliniko.com/v1/bookings?`, {
-            headers: {
-                'Authorization': `Basic ${encodedApiKey}`,
-                'Accept': 'application/json',
-                'User-Agent': 'Cliniko-to-Whatsapp API (ianchiro.frontdesk@gmail.com)',
-            },
-        });
+      const encodedApiKey = btoa(`${clinikoApiKey}:`);
     
-        // Log the response data to see if the fetch is successful
-        console.log('Bookings fetched successfully:', response.data);
-        return response.data.bookings;
+      const response = await axios.get(`https://api.au2.cliniko.com/v1/individual_appointments?q[]=starts_at:>=${endDate}T00:00:00Z&q[]=starts_at:<=${endDate} T23:59:59Z`,{ //the endpoint of the API for tomorrow's date
+          headers: {
+              'Authorization': `Basic ${encodedApiKey}`,
+              'Accept': 'application/json',
+              'User-Agent': 'Cliniko-to-Whatsapp API (ianchiro.frontdesk@gmail.com)',
+          }, //Cliniko's basic Authentication
+      });
+  
+      // Log the response data to see if the fetch is successful
+      // data.individual_appointments is an array (try to fetch the patient_name, id, start_at details)
+      const bookings = response.individual_appointments;
+      bookings.forEach(element => {
+        
+      });
+      console.log('Bookings fetched successfully:', response.data.individual_appointments);
+      // return response.data.bookings;
     } catch (error) {
         console.error('Error fetching bookings:', error.message);
         return null;
@@ -49,7 +45,7 @@ const fetchBookings = async () => {
   };
 
   const main = async () => {
-    await fetchBookings();
+    await fetchBookings(formattedTomorrow);
   };
   
   main(); // Run the main function
